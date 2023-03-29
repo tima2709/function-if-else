9a14bad3b739762a8713bfc1cc6ab439d9d834fd
tmrln2709 <tmrln2709@gmail.com>
2023-03-29 23:18:13 +0600

    HW done

--- script.js
+++ script.js
@@ -26,11 +26,14 @@
 //
 //         else {
 //             return `welcome, "${user}"! how are you? today is ${day}, simple day`
-//         }
+//         }r
 //     }
 // }
 //
 // console.log(handleSum('Tima', 23))
+
+// user admin true welcome, 'user'
+// false not correct user
 
 // function handleSum(user){
 //     if (user === 'admin'){
@@ -43,6 +46,9 @@
 //
 // console.log(handleSum('admin'))
 
+// 'num' is equals of 50
+// 'num' is not correct
+
 // function handleSum(num){
 //     if (num === 50){
 //         return 'correct'
@@ -54,28 +60,39 @@
 //
 // console.log(handleSum(15))
 
+// today is 'day' of february
+// on february only 28 day, your day is 'day' of another month
+
 // function handleSum(day){
 //     if (day <= 28){
 //         return `today is ${day} of February`
 //     }
-//     else {
+//     else if (day === 29 || day === 30 || day === 31) {
 //         return `on february only 28 day, your day is ${day} of another month`
+//     } else if (day > 32){
+//         return 'in month max 31 days'
 //     }
 //
 // }
 //
 // console.log(handleSum(27))
 
+// 'user' 'age' old, welcome
+// 'user' 'age' old, rejected
+
 // function handleSum(user, age){
 //     if (age > 18){
-//         return `${user} ${age} old, welcome`
+//         return `${user} ${age} years old, welcome`
 //     }
 //     else {
-//         return `${user} ${age} old, rejected`
+//         return `${user} ${age} years old, rejected`
 //     }
 // }
 //
 // console.log(handleSum("ahiles", 21))
+
+// 'user' name is too short (min 3)
+// 'user' name is accepted
 
 // function handleSum(user){
 //     if (user.length < 3){
@@ -87,6 +104,9 @@
 // }
 //
 // console.log(handleSum('jim'))
+
+// 'user' name is too long (max 5)
+// 'user' name is accepted
 
 // function handleSum(user){
 //     if (user.length > 5) {
@@ -109,10 +129,25 @@
 //     else {
 //         return `${age} is not accepted`
 //     }
-//
 // }
 //
 // console.log(handleSum(20))
+
+// 'age' must be a number ('20') - там просто проверка на тип аргумента, но не на само число.
+
+// function string(age){
+//     if (typeof (age) === 'string'){
+//         return `type of argument must be number`
+//     }
+//     else {
+//         return 'accepted'
+//     }
+// }
+//
+// console.log(string(25))
+
+// 'age' must be a string (20)
+// 'age' must be older then 23
 
 // function string(age){
 //     if (typeof (age) === 'number'){
@@ -122,14 +157,19 @@
 //         return `${age} is accepted`
 //     }
 //     else {
-//         return `${age} is not accepted`
+//         return `${age} years old is not accepted, age must be older then 23`
 //     }
 // }
 //
-// console.log(string('25'))
+// console.log(string('21'))
 
 // function dayoftheweek(today){
-//     if (today === 1) {
+//     if (today > 7){
+//         return 'There is no such a day'
+//     } else if (today > 5){
+//         return 'Today is weekend'
+//     }
+//     else if (today === 1) {
 //         return 'Today is monday'
 //     }
 //     else if (today === 2) {
@@ -144,16 +184,10 @@
 //     else if (today === 5) {
 //         return 'Today is friday'
 //     }
-//     else if (today === 6) {
-//         return 'Today is weekend'
-//     }
-//     else if (today === 7) {
-//         return 'Today is weekend'
-//     }
-//     else if (today > 7) {
-//         return 'there no such a day'
+//     else if (today === 0){
+//         return 'zero means that you are dead'
 //     }
 //
 // }
 //
-// console.log(dayoftheweek(8))
+// console.log(dayoftheweek(0))
